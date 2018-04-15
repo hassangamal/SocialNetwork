e493ee6b0aa1c460e41c76e0f2c265c2f1ddb696
hassangamal <gamalhassan807@yahoo.com>
2018-04-15 03:23:49 +0200

    login&signup

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <title>@yield('title')</title>
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+
+</body>
+</html>
